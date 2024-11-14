691e9415e4d9f2fe3b94594dd8dc5483f9fe26ae
Victimmm <1325844611@qq.com>
2024-11-14 14:53:58 +0800

    添加环境变量读取参数

--- checkin.py
+++ checkin.py
@@ -5,6 +5,7 @@
 """
 import requests
 import re
+import os
 
 def check_in(cookies):
     check_in_url = "https://neworld.cloud/user/checkin"
@@ -60,7 +61,7 @@ def login(user, password):
     response = requests.post(url, headers=headers, data=data, verify=False)
     # 将Set-Cookie头拆分为单个cookie条目
     entries = response.headers.get('Set-Cookie')
-    keys_to_extract = ["uid", "key", "email", "expire_in"] 
+    keys_to_extract = ["uid", "key", "email", "expire_in"]
 
     # 创建一个正则表达式模板，匹配每个目标键值对
     cookie_pattern = r'(?P<key>' + '|'.join(keys_to_extract) + r')=([^;]+)'
@@ -72,6 +73,9 @@ def login(user, password):
 
     return cookie_dict
 
+# 环境变量中读取数据，包含账号密码，和登陆页面测试
+user = os.environ["USERNAME"]
+password = os.environ["PASSWORD"]
 
 add_cookies = {"intercom-device-id-sh7mjuq3": "88077d2f-4636-4e2e-8317-c2be2ca5e4dc", "intercom-id-sh7mjuq3": "b559e5c3-1f87-4f32-92a4-ea64a84c82ef", "_pk_id.1.86b2": "9c41cc0eac5d13af.1684203936."}
 cookies = login(user, password)
